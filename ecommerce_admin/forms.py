f57c1afe8ae1610bc351410b0328c16c4f9e0d2e
sathish <sathish323334@gmail.com>
2025-07-24 15:34:11 +0530

    fgfg

--- ecommerce_admin/forms.py
+++ ecommerce_admin/forms.py
@@ -1,14 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, FloatField, TextAreaField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField, FloatField
+from wtforms.validators import DataRequired, Email, Length, NumberRange
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    submit = SubmitField('Register')
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
 
-class ProductForm(FlaskForm):
-    name = StringField('Product Name', validators=[DataRequired()])
-    price = FloatField('Price', validators=[DataRequired()])
-    description = TextAreaField('Description')
-    submit = SubmitField('Save')
+class ReviewForm(FlaskForm):
+    rating = FloatField('Rating (0.0 - 5.0)', validators=[DataRequired(), NumberRange(min=0.0, max=5.0)])
+    comment = TextAreaField('Comment', validators=[DataRequired(), Length(min=10)])
+    submit = SubmitField('Submit Review')
--- ecommerce_admin/models.py
+++ ecommerce_admin/models.py
@@ -1,24 +1,17 @@
-# models.py
-
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 
 db = SQLAlchemy()
 
-class User(db.Model, UserMixin):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True, nullable=False)
+    username = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
-    reviews = db.relationship('Review', backref='user', lazy=True)
-
-class Product(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    price = db.Column(db.Float, nullable=False)
-    description = db.Column(db.Text)
+    reviews = db.relationship('Review', backref='author', lazy=True)
 
 class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    rating = db.Column(db.Integer, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
     comment = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
